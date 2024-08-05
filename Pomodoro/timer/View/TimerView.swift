23c6e0a6829ac190adba29034ebef339d6c098c1
lsimon <leo@lsimon.org>
2024-08-05 12:17:40 -0400

    MVVM: Remove UIKit from TimerViewModel

--- Pomodoro/timer/View/TimerView.swift
+++ Pomodoro/timer/View/TimerView.swift
@@ -16,17 +16,27 @@ class TimerView: UIView {
     @IBOutlet weak var upNextImage: UIImageView!
     @IBOutlet weak var upNextLabel: UILabel!
     
+    static let workImage = UIImage(systemName: "laptopcomputer") ?? UIImage(named: "laptopcomputer")!
+    static let breakImage = UIImage(systemName: "cup.and.saucer") ?? UIImage(named: "cup.and.saucer")!
+    static let forwardIcon = UIImage(systemName: "forward.end") ?? UIImage(named: "forward.end")!
+    static let playIcon = UIImage(systemName: "play") ?? UIImage(named: "play")!
+    //    https://www.hackingwithswift.com/example-code/language/when-is-it-safe-to-force-unwrap-optionals
+    
     let viewModel = TimerViewModel()
-
+    
     @IBAction func toggleTimer(_ sender: Any) {
         viewModel.toggleTimer()
     }
-
+    
     func configure(cycle: Cycle) {
         viewModel.configure(cycle: cycle, uiUpdateDelegate: self)
         updateTimerLabel(with: TimeFormatter.secToTimeStr(for: viewModel.time))
     }
-
+    
+    func getImage(for name: String) -> UIImage {
+        return UIImage(systemName: name) ?? UIImage(named: name)!
+    }
+    
     func clean() {
         viewModel.clean()
     }
@@ -34,10 +44,10 @@ class TimerView: UIView {
 
 protocol TimerViewUIUpdateDelegate {
     func updateTimerLabel(with newLabel: String)
-    func updateControlBtnTitle(text: String, image: UIImage, color: UIColor)
-    func updateTimer(image: UIImage, color: UIColor)
+    func updateControlBtnTitle(text: String, image: String, rgb: String)
+    func updateTimer(image: String, rgb: String)
     
-    func showUpNext(image: UIImage, text: String)
+    func showUpNext(image: String, tintColor: String, text: String)
     func hideUpNext()
 }
 
@@ -46,19 +56,29 @@ extension TimerView: TimerViewUIUpdateDelegate {
         timerLabel.text = newLabel
     }
     
-    func updateControlBtnTitle(text: String, image: UIImage, color: UIColor) {
+    func updateControlBtnTitle(text: String, image: String, rgb: String) {
+        let color = UIColor(rgb: rgb)
+        let img = TimerView.playIcon.withTintColor(color, renderingMode: .alwaysOriginal)
+        
         controlBtn.setTitle(text, for: .normal)
-        controlBtn.setImage(image, for: .normal)
+        controlBtn.setImage(img, for: .normal)
         controlBtn.setTitleColor(color, for: .normal)
     }
     
-    func updateTimer(image: UIImage, color: UIColor) {
+    func updateTimer(image: String, rgb: String) {
+        let color = UIColor(rgb: rgb)
+        let image = getImage(for: image).withTintColor(color)
+        
+        
         timerImage.image = image
         timerBgView.backgroundColor = color
     }
     
-    func showUpNext(image: UIImage, text: String) {
-        upNextImage.image = image
+    func showUpNext(image: String, tintColor: String, text: String) {
+        let color = UIColor(rgb: tintColor)
+        let img = getImage(for: image).withTintColor(color, renderingMode: .alwaysOriginal)
+        
+        upNextImage.image = img
         upNextLabel.text = text
         upNextView.layer.opacity = 1
     }
--- Pomodoro/timer/ViewModel/TimerViewModel.swift
+++ Pomodoro/timer/ViewModel/TimerViewModel.swift
@@ -5,34 +5,25 @@
 //  Created by Leo on 2024-07-01.
 //
 
-import UIKit
 import AVFoundation
 
 final class TimerViewModel: NSObject {
-    
-    static let workImage = UIImage(systemName: "laptopcomputer") ?? UIImage(named: "laptopcomputer")!
-    static let breakImage = UIImage(systemName: "cup.and.saucer") ?? UIImage(named: "cup.and.saucer")!
-    static let forwardIcon = UIImage(systemName: "forward.end") ?? UIImage(named: "forward.end")!
-    static let playIcon = UIImage(systemName: "play") ?? UIImage(named: "play")!
-
-    //    https://www.hackingwithswift.com/example-code/language/when-is-it-safe-to-force-unwrap-optionals
-
     var uiUpdateDelegate: TimerViewUIUpdateDelegate?
     var timers: [CycleTimer] = []
     var colors: CycleColors?
-
+    
     var breakAudioPlayer: AVAudioPlayer?
     var workAudioPlayer: AVAudioPlayer?
-
+    
     var currentTimer: CycleTimer {
         return timers[timerIndex]
     }
-
+    
     var nextTimer: CycleTimer {
         let i = timerIndex == timers.count - 1 ? 0 : timerIndex + 1
         return timers[i]
     }
-
+    
     var time = 0 {
         didSet {
             uiUpdateDelegate?.updateTimerLabel(with: TimeFormatter.secToTimeStr(for: time))
@@ -43,58 +34,55 @@ final class TimerViewModel: NSObject {
             }
         }
     }
-
+    
     var timerIndex = 0
     var timer = Timer()
-
+    
     func configure(cycle: Cycle, uiUpdateDelegate: TimerViewUIUpdateDelegate) {
         self.timers = cycle.timers
         self.colors = cycle.colors
         self.uiUpdateDelegate = uiUpdateDelegate
-
+        
         time = currentTimer.duration
-
+        
         updateTimerUI()
         
-        let color = UIColor(rgb: cycle.colors.work )
-        let img = TimerViewModel.playIcon.withTintColor(color, renderingMode: .alwaysOriginal)
-        uiUpdateDelegate.updateControlBtnTitle(text: "", image: img, color: UIColor(rgb: cycle.colors.work))
-
+        uiUpdateDelegate.updateControlBtnTitle(text: "", image: "play", rgb: cycle.colors.work)
+        
+        uiUpdateDelegate.updateControlBtnTitle(text: "", image: "play", rgb: cycle.colors.work)
         let breakSoundUrl = Bundle.main.url(forResource: "break", withExtension: "wav")!
-
+        
         let workSoundUrl = Bundle.main.url(forResource: "work", withExtension: "wav")!
-
+        
         workAudioPlayer = try? AVAudioPlayer(contentsOf: workSoundUrl)
         breakAudioPlayer = try? AVAudioPlayer(contentsOf: breakSoundUrl)
     }
-
+    
     func clean() {
         if timer.isValid {
             timer.invalidate()
         }
     }
-
+    
     func updateTimerUI() {
         assert(uiUpdateDelegate != nil && colors != nil, "This viewModel is missing some important dependancies, did you forget to call it's configure method?")
         guard let uiUpdateDelegate = uiUpdateDelegate, let colors = colors else {
             print("This viewModel is missing some important dependancies, did you forget to call it's configure method?")
             return
         }
-
+        
         uiUpdateDelegate.hideUpNext()
         if (currentTimer.type == .work) {
-            let color = colors.work
-            uiUpdateDelegate.updateTimer(image: TimerViewModel.workImage, color: UIColor(rgb: color))
+            uiUpdateDelegate.updateTimer(image: "laptopcomputer", rgb: colors.work)
         } else {
-            let color = colors.pause
-            uiUpdateDelegate.updateTimer(image: TimerViewModel.breakImage, color: UIColor(rgb: color))
+            uiUpdateDelegate.updateTimer(image: "cup.and.saucer", rgb: colors.pause)
         }
     }
-
+    
     @objc func tick() {
         time -= 1
     }
-
+    
     func toggleTimer() {
         if (timer.isValid) {
             skipToNextTimer()
@@ -102,50 +90,47 @@ final class TimerViewModel: NSObject {
             startTimer()
         }
     }
-
+    
     func startTimer() {
         assert(uiUpdateDelegate != nil && colors != nil, "This viewModel is missing some important dependancies, did you forget to call it's configure method?")
         guard let uiUpdateDelegate = uiUpdateDelegate, let colors = colors else {
             print("This viewModel is missing some important dependancies, did you forget to call it's configure method?")
             return
         }
-
+        
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
-
+        
         let buttonText = currentTimer.type == .work ? "Skip to the break" : "Skip to work"
-        let color = currentTimer.type == .work ? UIColor(rgb: colors.work) : UIColor(rgb: colors.pause)
-        let img = TimerViewModel.forwardIcon.withTintColor(color, renderingMode: .alwaysOriginal)
-        uiUpdateDelegate.updateControlBtnTitle(text: buttonText, image: img, color: color)
-
+        let color = currentTimer.type == .work ? colors.work : colors.pause
+        uiUpdateDelegate.updateControlBtnTitle(text: buttonText, image: "forward.end", rgb: color)
+        
         let timerText = "\(nextTimer.name): \(TimeFormatter.secToTimeStr(for: nextTimer.duration))"
-        let image = nextTimer.type == .work ? TimerViewModel.workImage : TimerViewModel.breakImage
-        let tintColor = nextTimer.type == .work ? UIColor(rgb: colors.work) : UIColor(rgb: colors.pause)
-        let imageWithTint = image.withTintColor(tintColor, renderingMode: .alwaysOriginal)
-        uiUpdateDelegate.showUpNext(image: imageWithTint, text: timerText)
+        let image = nextTimer.type == .work ? "laptopcomputer" : "cup.and.saucer"
+        let tintColor = nextTimer.type == .work ? colors.work : colors.pause
+        uiUpdateDelegate.showUpNext(image: image, tintColor: tintColor, text: timerText)
     }
-
+    
     func skipToNextTimer() {
         assert(uiUpdateDelegate != nil && colors != nil, "This viewModel is missing some important dependancies, did you forget to call it's configure method?")
         guard let uiUpdateDelegate = uiUpdateDelegate, let colors = colors else {
             print("This viewModel is missing some important dependancies, did you forget to call it's configure method?")
             return
         }
-
+        
         timerIndex = timerIndex == timers.count - 1 ? 0 : timerIndex + 1
-
+        
         timer.invalidate()
-
-        let color = currentTimer.type == .work ? UIColor(rgb: colors.work) : UIColor(rgb: colors.pause)
-        let img = TimerViewModel.playIcon.withTintColor(color, renderingMode: .alwaysOriginal)
-
-        uiUpdateDelegate.updateControlBtnTitle(text: "", image: img, color: color)
+        
+        let color = currentTimer.type == .work ? colors.work : colors.pause
+        
+        uiUpdateDelegate.updateControlBtnTitle(text: "", image: "play", rgb: color)
         time = currentTimer.duration
         updateTimerUI()
     }
-
+    
     func playTimerSound() {
         let player = currentTimer.type == .work ? workAudioPlayer : breakAudioPlayer
-
+        
         player?.prepareToPlay()
         player?.play()
     }
