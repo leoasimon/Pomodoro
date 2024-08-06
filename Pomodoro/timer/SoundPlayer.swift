f3021b3488e88b18cd0f9e908157e6b2f6ac5a3b
lsimon <leo@lsimon.org>
2024-08-06 08:47:21 -0400

    play sound: separate module

---
+++ Pomodoro/timer/SoundPlayer.swift
@@ -0,0 +1,29 @@
+//
+//  SoundPlayer.swift
+//  Pomodoro
+//
+//  Created by Leo on 2024-08-05.
+//
+
+import Foundation
+import AVFoundation
+
+// TODO: test (no earbuds and no private space so far)
+final class SoundPlayer {
+    static private let breakSoundUrl = Bundle.main.url(forResource: "break", withExtension: "wav")!
+
+    static private let workSoundUrl = Bundle.main.url(forResource: "work", withExtension: "wav")!
+
+    static private let workAudioPlayer = try? AVAudioPlayer(contentsOf: workSoundUrl)
+    static private let breakAudioPlayer = try? AVAudioPlayer(contentsOf: breakSoundUrl)
+    
+    static func playWorkSound() {
+        workAudioPlayer?.prepareToPlay()
+        workAudioPlayer?.play()
+    }
+    
+    static func playBreakSound() {
+        breakAudioPlayer?.prepareToPlay()
+        breakAudioPlayer?.play()
+    }
+}
